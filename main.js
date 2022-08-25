633647cd5ce81d41d3384b1dd1ffe22b5b231b36
vigneshwaranBS <bs.vigneshwaran@gmail.com>
2022-08-25 22:00:35 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Javascript advanced topic");
+
+// console.log(window);
+
+// Functions
+function addnum(num1, num2) {
+  console.log(num1 + num2);
+}
+
+addnum(26, 8);
+
+const a = add(5, 5);
+
+function add(add1, add2) {
+  return add1 * add2;
+}
+
+console.log(a);
